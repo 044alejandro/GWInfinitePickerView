c36c5608ffc067768de98a477fafbb846c97ca40
Aleksandra Rusakovskaya <sasbieb@gmail.com>
2025-08-07 02:08:31 +0200

    Add publicHeadersPath for SwiftPM

--- Package.swift
+++ Package.swift
@@ -2,21 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "GWInfinitePickerView",          // имя пакета
-    platforms: [
-      .iOS(.v9)                            // поддержка iOS 9.0 и выше
-    ],
-    products: [
-      .library(
-        name: "GWInfinitePickerView",      // имя библиотеки для подключения
-        targets: ["GWInfinitePickerView"]
-      )
-    ],
-    targets: [
-      .target(
-        name: "GWInfinitePickerView",      // цель для сборки
-        path: "GWInfinitePickerView"       // папка с исходниками
-      )
-    ]
+  name: "GWInfinitePickerView",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "GWInfinitePickerView", targets: ["GWInfinitePickerView"])
+  ],
+  targets: [
+    .target(
+      name: "GWInfinitePickerView",
+      path: "GWInfinitePickerView",
+      publicHeadersPath: "."
+    )
+  ]
 )
-
